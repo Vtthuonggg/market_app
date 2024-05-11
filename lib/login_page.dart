03085237264287dce08a516611bc8f0042cf81f0
Vu Thanh Thuong <thuong23042003@gmail.com>
2024-05-12 00:43:16 +0700

    bate cinema

---
+++ lib/login_page.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app/home_page.dart';
+import 'package:flutter_app/resources/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nylo_framework/nylo_framework.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _controller = TextEditingController();
+  TextEditingController _controller2 = TextEditingController();
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight),
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 0, 90, 164),
+                  Color.fromARGB(255, 139, 203, 255)
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+            ),
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              title: const Text(
+                'Đăng nhập',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: SafeArea(
+              child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    width: 150,
+                    height: 150,
+                    child: Image.asset('public/assets/images/logo_bate.png')),
+                TextField(
+                  controller: _controller,
+                  onTapOutside: (event) {
+                    FocusScope.of(context).unfocus();
+                  },
+                  cursorColor: Colors.blue,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 15),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.grey[400]!)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.blue)),
+                    hintText: 'Email hoặc tên đăng nhập',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    prefixIcon:
+                        Icon(Icons.email_outlined, color: Colors.grey[400]!),
+                    prefix: VerticalDivider(color: Colors.green),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  controller: _controller2,
+                  onTapOutside: (event) {
+                    FocusScope.of(context).unfocus();
+                  },
+                  cursorColor: Colors.blue,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 15),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.grey[400]!)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.blue)),
+                    hintText: 'Mật khẩu',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    prefixIcon: Icon(Icons.lock_open_outlined,
+                        color: Colors.grey[400]!),
+                    prefix: VerticalDivider(color: Colors.green),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      'Quên mật khẩu?',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          color: Colors.blue[900],
+                          decoration: TextDecoration.underline),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Container(
+                    width: width,
+                    height: height / 15,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 229, 88, 0),
+                          Color.fromARGB(255, 242, 198, 76)
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomePageUser()),
+                        );
+                      },
+                      child: Text('ĐĂNG NHẬP',
+                          style: GoogleFonts.oswald(
+                              fontSize: 20, color: Colors.white)),
+                    )),
+                SizedBox(height: 20),
+                Container(
+                    width: width,
+                    height: height / 15,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.black),
+                    child: Center(
+                      child: Text('ĐĂNG NHẬP BẰNG GOOGLE',
+                          style: GoogleFonts.oswald(
+                              fontSize: 20, color: Colors.white)),
+                    )),
+                SizedBox(height: 20),
+                Container(
+                    width: width,
+                    height: height / 15,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 0, 94, 255),
+                          Color.fromARGB(255, 131, 193, 251)
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text('ĐĂNG NHẬP BẰNG FACEBOOK',
+                          style: GoogleFonts.oswald(
+                              fontSize: 20, color: Colors.white)),
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(left: 10.0, right: 15.0),
+                        child: Divider(
+                          color: Colors.grey,
+                          height: 50,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Hoặc",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(left: 15.0, right: 10.0),
+                        child: Divider(
+                          color: Colors.grey,
+                          height: 50,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Text('Đăng ký tài khoản Bate Cinema',
+                    style: GoogleFonts.oswald(
+                        fontSize: 20, color: Colors.grey[700])),
+              ],
+            ),
+          )),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,56 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import '/bootstrap/app.dart';
-import 'package:nylo_framework/nylo_framework.dart';
-import 'bootstrap/boot.dart';
+import 'package:flutter_app/login_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  Nylo nylo = await Nylo.init(setup: Boot.nylo, setupFinished: Boot.finished);
+void main() {
+  runApp(const MyApp());
+}
 
-  runApp(
-    AppBuild(
-      navigatorKey: NyNavigator.instance.router.navigatorKey,
-      onGenerateRoute: nylo.router!.generator(),
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: nylo.getInitialRoute(),
-      navigatorObservers: nylo.getNavigatorObservers(),
-    ),
-  );
+      home: SplashScreen(),
+      routes: {
+        '/login': (context) => LoginPage(),
+      },
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Transform.scale(
+              scale: 0.5, child: Image.asset('public/assets/images/bate.png')),
+          CircularProgressIndicator(
+            color: Colors.blue,
+          )
+        ],
+      ),
+    );
+  }
 }
